74605184ddf6081266df330fb74f20ae5abf24dd
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-03-03 13:05:09 +0100

    Update cloud/azure/locals.tf
    Co-Authored-By: Patrick Decat <pdecat@gmail.com>

--- cloud/azure/locals.tf
+++ cloud/azure/locals.tf
@@ -1,4 +1,3 @@
 locals {
-  sfx_suffix               = var.sfx_integration_name_suffix == "" ? "" : format("-%s", var.sfx_integration_name_suffix)
-  default_integration_name = format("AzureSignalFxIntegration%s", local.sfx_suffix)
+  default_integration_name = format("AzureSignalFxIntegration%s", var.sfx_integration_name_suffix)
 }
