0277b2b5bf15f16507bc7acc9aad47a7fca8a8f4
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-03-27 09:54:14 +0100

    SFX-3 fix output for pagerduty

--- alerting/pagerduty/outputs.tf
+++ alerting/pagerduty/outputs.tf
@@ -1,4 +1,4 @@
 output "sfx_integration_id" {
   description = "SignalFx integration ID"
-  value       = signalfx_pagerduty_integration.sfx_integration.integration_id
+  value       = signalfx_pagerduty_integration.sfx_integration.id
 }
