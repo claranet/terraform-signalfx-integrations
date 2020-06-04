d6fc482962e9aaae1f5f460a15a89a84d11408be
Patrick Decat <patrick.decat@fr.clara.net>
2020-06-04 13:19:50 +0200

    Add the SignalFx named token name/secret to outputs

--- cloud/gcp/outputs.tf
+++ cloud/gcp/outputs.tf
@@ -2,3 +2,10 @@ output "gcp_service_account_id" {
   description = "The GCP service account id used by SignalFx"
   value       = local.id_to_use
 }
+
+output "signalfx_named_token" {
+  description = "The SignalFx named token used by the GCP integration"
+  value = {
+    (signalfx_org_token.gcp_claranet_integration.name) = signalfx_org_token.gcp_claranet_integration.secret
+  }
+}
