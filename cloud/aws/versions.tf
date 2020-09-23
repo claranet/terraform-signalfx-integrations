94c263c978ae675811d09b42ba56e38512871050
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-09-23 13:11:06 +0200

    compatibility for terraform 0.13

--- cloud/aws/versions.tf
+++ cloud/aws/versions.tf
@@ -1,9 +1,9 @@
-
 terraform {
-  required_version = "~> 0.12"
   required_providers {
-    aws      = "~> 2"
-    signalfx = "~> 4"
+    signalfx = {
+      source  = "splunk-terraform/signalfx"
+      version = ">= 4.26.4"
+    }
   }
+  required_version = ">= 0.12.26"
 }
-
