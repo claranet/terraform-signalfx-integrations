6fb4f11856f98f72ac5f67d0502764bce1f256f1
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-09-28 15:46:23 +0200

    add provider requirements for cloud integrations

--- cloud/aws/versions.tf
+++ cloud/aws/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "splunk-terraform/signalfx"
       version = ">= 4.26.4"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2"
+    }
   }
   required_version = ">= 0.12.26"
 }
--- cloud/azure/versions.tf
+++ cloud/azure/versions.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "splunk-terraform/signalfx"
       version = ">= 4.26.4"
     }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 1.44"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = ">= 0.8"
+    }
   }
   required_version = ">= 0.12.26"
 }
