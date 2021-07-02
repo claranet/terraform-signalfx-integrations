2ca1e56038535ee6f4a198a98f701ea2ee74ac9d
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-07-02 11:51:58 +0200

    add wait workaround to prevent aws permission denied on role

--- cloud/aws/roles.tf
+++ cloud/aws/roles.tf
@@ -8,3 +8,11 @@ resource "aws_iam_role_policy_attachment" "sfx_policy_attach" {
   role       = aws_iam_role.sfx_role.name
   policy_arn = aws_iam_policy.sfx_policy.arn
 }
+
+# workaround to be sure the policy is really applied to the role before to try to create the integration
+resource "time_sleep" "policy_availability" {
+  depends_on = [aws_iam_role_policy_attachment.sfx_policy_attach]
+
+  create_duration = "15s"
+}
+
