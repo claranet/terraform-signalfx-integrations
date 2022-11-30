00b62e3c017d5ed63bfc1535342e4e7e56c67897
Charles Vauvarin <charles.vauvarin@fr.clara.net>
2022-11-30 09:09:11 +0100

    fmt

--- cloud/aws/locals.tf
+++ cloud/aws/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  integration_name = "AWSIntegration${var.suffix == "" ? "" : "-${title(var.suffix)}"}"
-  all_services = data.signalfx_aws_services.aws_services.services[*].name
+  integration_name  = "AWSIntegration${var.suffix == "" ? "" : "-${title(var.suffix)}"}"
+  all_services      = data.signalfx_aws_services.aws_services.services[*].name
   excluded_services = concat(var.excluded_services, ["AWS/EC2"])
-  aws_services = coalescelist(tolist(setintersection(var.included_services, local.all_services)), local.all_services)
+  aws_services      = coalescelist(tolist(setintersection(var.included_services, local.all_services)), local.all_services)
 }
