bc8d18b6a6c14e415b78589b74b4a2e5b271032a
Patrick Decat <patrick.decat@fr.clara.net>
2023-08-22 15:49:57 +0200

    fix(aws): add constraint for terraform time provider

--- cloud/aws/versions.tf
+++ cloud/aws/versions.tf
@@ -1,13 +1,20 @@
 terraform {
   required_providers {
-    signalfx = {
-      source  = "splunk-terraform/signalfx"
-      version = ">= 8.1.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0.0"
     }
+
+    signalfx = {
+      source  = "splunk-terraform/signalfx"
+      version = ">= 8.1.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
+    }
   }
+
   required_version = ">= 1.0"
 }
