4184343571d7ffc361d4e420182c4fcca8dbf031
Jérôme Respaut <jerome.respaut@fr.clara.net>
2023-10-24 17:02:20 +0200

    Update cloud/azure/versions.tf
    Co-authored-by: Patrick Decat <patrick.decat@fr.clara.net>

--- cloud/azure/versions.tf
+++ cloud/azure/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     signalfx = {
       source  = "splunk-terraform/signalfx"
-      version = "~> 8.0"
+      version = ">= 8.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
